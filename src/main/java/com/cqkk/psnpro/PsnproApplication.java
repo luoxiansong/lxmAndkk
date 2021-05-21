73ebc7d00afe292881bcbea1aa5a9f04f29b964f
luoxiansong <956402755@qq.com>
2021-05-21 22:43:58 +0800

    push code

---
+++ src/main/java/com/cqkk/psnpro/PsnproApplication.java
@@ -0,0 +1,13 @@
+package com.cqkk.psnpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsnproApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsnproApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cqkk/psnpro/PsnproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cqkk.psnpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsnproApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
