b3b36bf0d3800f6b69125a1e36473198a204218d
luoxiansong <956402755@qq.com>
2021-05-21 23:01:11 +0800

    测试提交 git是否成功 --20210521 by luokaka

---
+++ src/main/java/com/cqkk/psnpro/common/DateTools.java
@@ -0,0 +1,23 @@
+package com.cqkk.psnpro.common;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @program: psnpro
+ * @description:
+ * @author: Mr.Luo
+ * @create: 2021-05-21 22:54
+ **/
+public class DateTools {
+
+    public static String getNowDate() {
+        String nowDate = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        return nowDate;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("当前日期为:" + getNowDate());
+    }
+}
